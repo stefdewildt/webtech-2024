fa592af1ef8a498e47e2c520b7e4fe9d27b49073
isavdh <153298107+isavdh@users.noreply.github.com>
2024-01-22 18:22:20 +0100

    Update and rename php/add_profile_picture.php to uploadfile.php
    deze code zorgt ervoor dat een file geupload wordt als er op de knop submit wordt gedrukt

---
+++ uploadfile.php
@@ -0,0 +1,38 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $file = $_FILES['file'];
+
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName= $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+    $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+
+    if (in_array($fileActualExt, $allowed)){
+        if ($fileError ===0){
+            if($fileSize < 10000000){
+                $fileNameNew = iniqid('', true)."."."$fileActualExt";
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploades_file($fileTmpName, $fileDestination);
+                header("Location: add_profile_pic_test.php?uploadsuccess");
+                echo "your file has been uploaded!";
+            } else {
+                echo "your file is too big!";
+            }
+        } else {
+            echo "There was an error uploading your file";
+        }
+
+    } else {
+        echo "you cannot upload files of this type";
+    }
+}
+
+    session_start();
+    include_once 'dbh.php';
+?> 

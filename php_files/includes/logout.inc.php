68f1664d817987180253cf0c67e7971ffcc3109d
stefdewildt <stef.de.wildt@student.uva.nl>
2024-01-24 14:29:45 +0100

    spange opmaak

--- php_files/includes/logout.inc.php
+++ php_files/includes/logout.inc.php
@@ -5,5 +5,5 @@
     session_unset();
     session_destroy();
 
-    header('location:  https://webtech-bg2.webtech-uva.nl/~stefw/nieuw/webtech-2024/index.php');
+    header('location:  https://webtech-bg2.webtech-uva.nl/index.php');
 

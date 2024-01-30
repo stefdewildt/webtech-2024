f40b1dcf70c2bf3d09652fc474cc6a1566d82b5e
jellebwillems <122273509+jellebwillems@users.noreply.github.com>
2024-01-30 17:58:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -34,6 +34,15 @@
                 //    $username = $conn->query($sql);
                     
                   //  echo $username;
+                    $user_id = $row['user_id'];
+                    $sql = "SELECT usersUid FROM users WHERE usersId = $user_id";
+                    $result_user = mysqli_query($your_db_connection, $sql);
+                    $user_row = mysqli_fetch_assoc($result_user);
+                    $username = $user_row['usersUid'];
+
+                    // Output the username
+                    echo "Username: " . $username . "<br>";
+            
 
                     if (strpos($row['postsURL'], 'embed') !== true) {
                         $modifiedURL = str_replace('.com/', '.com/embed/', $row['postsURL']);

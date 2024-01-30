596a79436123739e9a3336f43bad72caa204735b
isavdh <153298107+isavdh@users.noreply.github.com>
2024-01-30 14:33:52 +0100

    kleine comment

---
+++ php_files/link_friends_new.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'includes/dbh.inc.php';
+
+//controleer of gebruiker is ingelogd
+session_start();
+if (isset($_SESSION['usersId'])){
+	//eigen id opvragen
+	$user_ID_1 = $_SESSION['usersId'];
+} else{
+//keer terug naar inlogpagina
+	header("Location: login.php");
+	exit();
+}
+
+// lalalala
+
+//haal de vriend-ID op uit het formulier 
+//het zou kunnen dat regel 19 nog niet werkt 
+if (!empty($_POST['volg_id'])) {
+	$user_ID_2 = 'volg_id';
+	//voeg vrienden toe aan een vriendenlijst
+	$sql = "INSERT INTO friends (user_ID_1, user_ID_2) VALUES ('$user_ID_1', '$user_ID_2')";
+}
+if ($conn->query($sql) === TRUE) {
+    echo "Vrienden succesvol gelinkt!";
+} else {
+    echo "Fout bij het linken van vrienden: " . $conn->error;
+}
+
+?>

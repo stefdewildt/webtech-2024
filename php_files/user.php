a32172e6e8cf79c78589d0bfc95646e0c06527bb
jellebwillems <122273509+jellebwillems@users.noreply.github.com>
2024-01-30 21:01:38 +0100

    css

--- php_files/user.php
+++ php_files/user.php
@@ -38,7 +38,7 @@
                 <form action="includes/uploadInc.php" class ="discussion-input" method="post">
                     <input type="text" name ="url" placeholder ="Paste Spotiy URL here">
                     <input type="text" name ="post" placeholder = "Start a conversation">
-                    <button type="Submit" name="submit">Submit Hot Take</button>
+                    <button class="button" type="Submit" name="submit">Submit Hot Take</button>
                 </form>
             </li>
         </ul>

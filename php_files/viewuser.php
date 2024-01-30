c3ff24c7f05249f2dd8561fd26b7a6c2351886d2
jellebwillems <122273509+jellebwillems@users.noreply.github.com>
2024-01-30 21:21:28 +0100

    Create viewuser.php

---
+++ php_files/viewuser.php
@@ -0,0 +1,75 @@
+<?php
+    include_once "header.php";
+    require_once '/var/www/dbhInc.php';
+// Check if the ID is provided in the URL
+    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+        $postId = $_GET['id'];
+
+        // Retrieve post data from the database based on the ID
+        $sql = "SELECT * FROM users WHERE id = $userUid";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $username = $row['useruid'];
+            $email = $row['useremail'];
+            $name = $row['username'];
+
+            // Display the post details
+            echo "<h2>$title</h2>";
+            echo "<p>Created at: $created_at</p>";
+            echo "<p>$content</p>";
+        } else {
+            echo "Post not found";
+        }
+    } else {
+        echo "Invalid or missing post ID";
+    }
+
+
+    
+$conn->close();
+
+?>
+<head>
+    <link rel="stylesheet" href="css_files/user_page.css">
+</head>
+
+    <div class="pd-row">
+        <img src="img/profile.png">
+        <div>
+            <h3><?php echo $name?></h3>
+            <h3><?php echo $username?></h3>
+            <h3><?php echo $email?></h3>
+            <nav>
+                <a href="/html/friends.html">120 friends</a>
+            </nav> 
+        </div>        
+    </div>
+
+    <div class="profile-posts">
+        <ul class="profile-post">
+            <li><h2>Your favourites</h2></li>
+              <div class="chosen-music">
+                  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1HAW56e0zz05phUnzuHF9E?utm_source=generator" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br>
+                  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/2ut4BOQSqxLpcX5MtPjzYa?utm_source=generator" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br>
+                  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/37Tmv4NnfQeb0ZgUC4fOJj?utm_source=generator" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br>
+                  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3Um9toULmYFGCpvaIPFw7l?utm_source=generator" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br>
+                  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3MyQn1xBQwPtFJUUP7zB8s?utm_source=generator" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br>
+              </div>
+            <li><h2>Your creations</h2></li>
+            <li>
+                <h2>Make a post:</h2>
+                <form action="includes/uploadInc.php" class ="discussion-input" method="post">
+                    <input type="text" name ="url" placeholder ="Paste Spotiy URL here">
+                    <input type="text" name ="post" placeholder = "Start a conversation">
+                    <button type="Submit" name="submit">Submit Hot Take</button>
+                </form>
+            </li>
+        </ul>
+    </div>
+
+<?php
+    include_once("footer.php");
+?>
+

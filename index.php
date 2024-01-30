a67c8a59567b9ac152d5a7879fb9532fab2b8925
jellebwillems <122273509+jellebwillems@users.noreply.github.com>
2024-01-30 13:21:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
                     } else {
                         $modifiedURL = $row['postsURL'];
                     }
-                    echo '<iframe style="border-radius: 12px" src="' . $modifiedURL . '" width="100%" height="100" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br>';
+                  //  echo '<iframe style="border-radius: 12px" src="' . $modifiedURL . '" width="100%" height="100" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br>';
 
 
                     echo "Hot take: " . $row['postsPOST'] . "<br>";

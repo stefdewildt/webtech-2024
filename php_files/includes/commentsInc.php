4fce2a357f3ab4c096d6ba8db3e21213aee470ff
WietskeBoersma <122273485+WietskeBoersma@users.noreply.github.com>
2024-01-31 22:58:07 +0100

    k

--- php_files/includes/commentsInc.php
+++ php_files/includes/commentsInc.php
@@ -63,7 +63,7 @@ function deleteComments($conn){
 
     $sql = "DELETE FROM comments WHERE cid='$cid'";
     $result = $conn->query($sql);
-    header("Location: index.php");
+    // header("Location: index.php");
     }
 }
 

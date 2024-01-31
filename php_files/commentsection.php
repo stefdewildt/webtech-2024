a5825b76be8f6635387816e6d8f44685af5b4abc
WietskeBoersma <122273485+WietskeBoersma@users.noreply.github.com>
2024-01-31 12:23:27 +0100

    comments

--- php_files/commentsection.php
+++ php_files/commentsection.php
@@ -10,12 +10,12 @@
 <!-- makes sure you have to be logged in to be able to comment -->
 <br><br>
 <?php
-    if (isset($_SESSION['id'])) {
+    if (isset($_SESSION['usersId'])) {
         echo "<form method='POST' action='".setComment($conn)."'>
         <input type='hidden' name='uid' value='anonymous'>
         <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
         <textarea name='message'></textarea><br>
-        <button type='submit' name='commentDelete'>Comment</button>
+        <button type='submit' name='commentSubmit'>Comment</button>
     </form>";
     } else {
         echo "You need to be logged in to comment!

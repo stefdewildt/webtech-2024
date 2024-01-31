e5ae30fcf97fe3c9245eac2dc3c93b7886866c14
WietskeBoersma <122273485+WietskeBoersma@users.noreply.github.com>
2024-01-31 15:31:19 +0100

    k

--- php_files/includes/commentsInc.php
+++ php_files/includes/commentsInc.php
@@ -20,29 +20,31 @@ function getComments($conn) {
     $sql = "SELECT * FROM comments";
     $result = $conn->query($sql);
     while ($row = $result->fetch_assoc()) {
-        $usersId = $row['usersId'];
-        $sql2 = "SELECT usersUid FROM users WHERE usersId = $usersId";
-        $result2 = $conn->query($sql2);
-        if ($row2 = $result2->fetch_assoc()) {
-            echo "<div class='comment-section'><p>";
-            echo $row2['usersUid'] . "<br>";
-            echo $row['date']. "<br><br>";
+        if (isset($row['usersId'])) {
+            $usersId = $row['usersId'];
+            $sql2 = "SELECT usersUid FROM users WHERE usersId = $usersId";
+            $result2 = $conn->query($sql2);
+            if ($row2 = $result2->fetch_assoc()) {
+                echo "<div class='comment-section'><p>";
+                echo $row2['usersUid'] . "<br>";
+                echo $row['date']. "<br><br>";
 
-            // checking for new line tags and make it into line breaks
-            echo nl2br($row['message']);
+                // checking for new line tags and make it into line breaks
+                echo nl2br($row['message']);
 
-            // able to delete a comment
-            echo "</p>";
-            if (isset($_SESSION['usersId'])) {
-                // current user comparing to user who made the comment
-                if (($_SESSION['usersId']) == ($row2['usersId'])){
-                    echo "<form class= 'delete-form' method='POST' action='".deleteComments($conn)."'>
-                    <input type='hidden' name='cid' value='".$row['cid']."'>
-                    <button type='submit' name='commentDelete'>Delete</button>
-                </form>";
+                // able to delete a comment
+                echo "</p>";
+                if (isset($_SESSION['usersId'])) {
+                    // current user comparing to user who made the comment
+                    if (($_SESSION['usersId']) == ($row2['usersId'])){
+                        echo "<form class= 'delete-form' method='POST' action='".deleteComments($conn)."'>
+                        <input type='hidden' name='cid' value='".$row['cid']."'>
+                        <button type='submit' name='commentDelete'>Delete</button>
+                    </form>";
+                    }
                 }
+                echo "</div>";
             }
-            echo "</div>";
         }
     }
 }

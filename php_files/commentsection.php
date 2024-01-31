b752b4de2cdc2eb8412a8e8a749c76e97022cd39
WietskeBoersma <122273485+WietskeBoersma@users.noreply.github.com>
2024-01-31 13:15:23 +0100

    k

--- php_files/commentsection.php
+++ php_files/commentsection.php
@@ -12,7 +12,7 @@
 <?php
     if (isset($_SESSION['usersId'])) {
         echo "<form method='POST' action='".setComment($conn)."'>
-        <input type='hidden' name='usersId' value='usersId'>
+        <input type='hidden' name='usersId' value='".$_SESSION['id']."'>
         <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
         <textarea name='message'></textarea><br>
         <button type='submit' name='commentSubmit'>Comment</button>

499ad70b4b496a05b143090212fba3c6b843d0e7
WietskeBoersma <122273485+WietskeBoersma@users.noreply.github.com>
2024-01-31 13:13:07 +0100

    kleine

--- php_files/includes/commentsInc.php
+++ php_files/includes/commentsInc.php
@@ -14,7 +14,7 @@ function setComment($conn){
     }
 }
 
-$user_id = $_SESSION['usersId'];
+// $user_id = $_SESSION['usersId'];
 // getting comments from database to be able to show on website 
 function getComments($conn) {
     $sql = "SELECT * FROM comments";

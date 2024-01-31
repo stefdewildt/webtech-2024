7920e38f611bb90d89001a28c4d26c389fa71792
jellebwillems <122273509+jellebwillems@users.noreply.github.com>
2024-01-31 14:08:06 +0100

    Create admin_users.php

---
+++ php_files/admin_users.php
@@ -0,0 +1,34 @@
+<?php
+
+    include_once "header.php";
+    require_once '/var/www/dbhInc.php';
+
+    if(isset($_SESSION['admin']) == false || $_SESSION['admin'] !== 1 ){
+        header("Location: ../index.php");
+
+    }
+
+?>
+
+<section class="posts">
+    <h1>Admin: Friends</h1>
+                <?php
+                $sql = "SELECT * FROM users ORDER BY usersId DESC";
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo 'User Uid:  ' . $row['usersUid'] . '<br>';
+                    echo 'Users Name:  ' . $row['usersName'] . '<br>';
+                    echo 'Users Email: ' . $row['usersEmail'] . '<br><br>';
+                    echo 'Users adminstatus' . $row['admin'];
+
+
+                    // Output  username boven de embed
+                    echo '<form method="Post" action="includes/removeuserInc.php">';
+                    echo '<input type="hidden" name="user_id" value="' . $row['usersId'] . '">';
+                    echo '<button type="submit" name="remove_user">Remove User</button>';
+                    echo '</form>';
+
+                    echo "<hr>"; // Voeg een scheidingsteken toe tussen records
+                }
+                ?>
+            </section>

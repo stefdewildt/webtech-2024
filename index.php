54a726e61e535e5c46f07a74c112cbfb43515226
jellebwillems <122273509+jellebwillems@users.noreply.github.com>
2024-01-31 18:06:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
                         // Output de post onder de embed
                         echo htmlspecialchars($row['postsPOST'], ENT_QUOTES, 'UTF-8');
                         // Voeg andere velden toe zoals nodig
-                        echo"<form method='POST' action='".setComment($conn)."'>
+                        echo"<form method='POST' action='".setComment($conn,$row['postsID'])."'>
                         <input type='hidden' name='usersId' value='".$_SESSION['usersId']."'>
                         <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
                         <input type='hidden' name='postId' value='".$row['postsID']."'>

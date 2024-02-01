901c8cdc5ed183beac263f47ba69307bf9f4e2f4
isavdh <153298107+isavdh@users.noreply.github.com>
2024-02-01 11:21:22 +0100

    search_functie

--- php_files/header.php
+++ php_files/header.php
@@ -80,8 +80,9 @@
             ?>
                 <li><a href="https://webtech-bg2.webtech-uva.nl/php_files/friends.php">Friends</a></li>
                 <li>
-                    <form class="search-form">
-                        <input type="text" placeholder="Search friends, genres, artists...">
+                    <form class="search-form" method="POST">
+                        <input type="text" name="search" placeholder="Search friends, genres, artists...">
+                        <button type="submit" name="submit-search">Search</button>
                     </form>
                 </li>
             </ul>

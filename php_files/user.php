71f586e05da4585b99c945d4ee6c8d5c848610d1
stefdewildt <stef.de.wildt@student.uva.nl>
2024-02-01 20:49:09 +0100

    link modified

--- php_files/user.php
+++ php_files/user.php
@@ -65,7 +65,7 @@
 ?>
 
 <head>
-    <link rel="stylesheet" href="https://webtech-bg2.webtech-uva.nl/~stefw/webtech-2024/php_files/css_files/user_page.css">
+    <link rel="stylesheet" href="https://webtech-bg2.webtech-uva.nl/php_files/css_files/user_page.css">
 </head>
 
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>

3306f38e1675828c8f883f52f2d5abe6ebda3c3d
jellebwillems <122273509+jellebwillems@users.noreply.github.com>
2024-02-01 16:54:29 +0100

    Update pfp.php

--- php_files/pfp.php
+++ php_files/pfp.php
@@ -1,1 +1,32 @@
-<button onclick="toggleFollowUser(<?php echo $other_user_id; ?>, this)"> <?php echo $following ? 'Following' : 'Follow'; ?></button> 
+<?php
+    include_once "header.php";
+    require_once '/var/www/dbhInc.php';
+    include 'includes/commentsInc.php';
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+
+
+
+<button attribute="1" onclick="togglePfp()</button> 
+
+<script>
+function togglePfp(userId, buttonElement) {
+    $.ajax({
+        type: 'POST',
+        url: 'changepfp.php', // Het pad naar je PHP-script voor volgen
+        data: { userId: userId },
+        success: function(response) {
+                buttonElement.innerText = 'Following';
+            
+        },
+        error: function(error) {
+            // Handel fouten af
+            console.error('Fout bij het volgen van de gebruiker:', error);
+        }
+    });
+
+
+    
+}
+

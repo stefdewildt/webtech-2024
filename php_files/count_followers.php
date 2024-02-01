d2b394b6e3ee6ad8a306e6712cffa3d91940c685
isavdh <153298107+isavdh@users.noreply.github.com>
2024-02-01 16:47:21 +0100

    pawfj

--- php_files/count_followers.php
+++ php_files/count_followers.php
@@ -4,12 +4,23 @@
 
     $usersUid= $_GET['id'];
     $usersId = "SELECT usersId FROM users WHERE usersUid = '$usersUid'";
+    $result_usersId = $conn->query($usersId);
+
+    if ($result_usersId->num_rows>0){
+        $rowUserId = $result_userId->fetch_assoc();
+        $usersId = $rowUserId['usersId'];
+
+        $query_find_friends = "SELECT * FROM friends where user_ID_1 = $usersId";
+        $result_find_friends = $conn->query($query_find_friends);
+
+        $counter = $result_find_friends->num_rows;
+
+        echo $counter;
+
+
+    } else {
+        echo 'Could not find friends';
+    }
    
-    $query_find_friends = "SELECT * FROM friends where user_ID_1 = $usersId";
-    $result_find_friends = $conn->query($query_find_friends);
-
-    $counter = $result_find_friends->num_rows 
-
-    echo $counter
-
+    
   ?>

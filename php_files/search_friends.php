472efda982cab97a3663054d18ffdecad9e6cd9f
isavdh <153298107+isavdh@users.noreply.github.com>
2024-02-01 11:39:01 +0100

    idjfg

--- php_files/search_friends.php
+++ php_files/search_friends.php
@@ -5,17 +5,18 @@
 //profielen zoeken die overeenkomen met gegeven zoekterm 
 if (isset($_POST['submit-search'])){
     $search = mysql_real_escape_string($conn, $_POST['search']);
-    $sql = "SELECT * FROM users WHERE usersUid LIKE '%$search%' OR usersName LIKE '%$search%'";
+    $sql = "SELECT * FROM users WHERE usersUid = $search";
     $result = mysqli_query($conn, $sql);
     $queryResult = mysqli_num_rows($result);
 
 //profielen weergeven in vorm van link die overeenkomen met zoekterm 
     if($queryResult > 0 ){
-        while ($row = mysqli_fetch_assoc($result)){
-            echo '<a href="profiel.php?usersId=' . $row['usersId'] . '">' . $row['usersname'] . '</a><br>';
-        }
+
+        $row = mysqli_fetch_assoc($result);
+        
+        header("Location: user.php?id={$row['userUid']}");
+        exit();
     } else {
         echo "There are no results matching your search";
     }
-}
-?> 
+?>    while ($row = ){

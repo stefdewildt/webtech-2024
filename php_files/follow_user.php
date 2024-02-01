d687d077a695c1d2aa0e57216d8f68289e6bd7c5
isavdh <153298107+isavdh@users.noreply.github.com>
2024-02-01 19:32:06 +0100

    sldkfn

--- php_files/follow_user.php
+++ php_files/follow_user.php
@@ -2,18 +2,21 @@
     include_once "header.php";
     require_once '/var/www/dbhInc.php';
 
+    //Haal opgestuurde gegevens op 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //maak variabele $userid voor de user die bekeken wordt
+    //maak variabele $huidige_gebruiker_id voor user die ingelogd is
     $userId = $_POST['userId'];
     $huidige_gebruiker_id = $_SESSION['usersId'];
 
-    // Voeg hier de logica toe om de gebruiker te volgen in de database
-    // (bijv. voeg een nieuwe rij toe aan de friends-tabel)
-
+    //maak query die beide userids op dezelfde rij opzoekt in friends
     $query_check_follow = "SELECT * FROM friends WHERE user_ID_1 = $huidige_gebruiker_id AND user_ID_2 = $userId";
     $result_check_follow = $conn->query($query_check_follow);
 
+    //als de de huidige gebruiker ($huidige_gebruiker_id) nog geen volger is van de opgezochte gebruiker ($userId) wordt if-statements uitgevoerd
     if ($result_check_follow->num_rows == 0) {
-        // Voeg vrienden toe aan een vriendenlijst
+        // insert $huidige_gebruiker_id op plek user_ID_1 en $userId op plek user_ID_2
         $query_follow_user = "INSERT INTO friends (user_ID_1, user_ID_2) VALUES ($huidige_gebruiker_id, $userId)";
         
         if ($conn->query($query_follow_user) === TRUE) {

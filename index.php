caffd18b3cd3747a6b23f8969fe0c69b56822b47
jellebwillems <122273509+jellebwillems@users.noreply.github.com>
2024-02-01 12:40:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 
              // Voorkomen dat er teveel requests naar spotify gaan
 
-      //              echo '<iframe style="border-radius: 12px" src="' . $modifiedURL . '" width="100%" height="100" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br>';
+                    echo '<iframe style="border-radius: 12px" src="' . $modifiedURL . '" width="100%" height="100" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe><br>';
 
 
                     // echo $row['username'] . ": " . $row['postsPOST'] . "<br>";

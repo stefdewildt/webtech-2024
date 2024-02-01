ead9218011229c6a59dc20b6816e6f953520ee73
isavdh <153298107+isavdh@users.noreply.github.com>
2024-02-01 11:49:35 +0100

    SJf

--- php_files/search_friends.php
+++ php_files/search_friends.php
@@ -2,21 +2,24 @@
   include_once "header.php";
   require_once '/var/www/dbhInc.php';
 
-//profielen zoeken die overeenkomen met gegeven zoekterm 
-if (isset($_POST['submit-search'])){
-    $search = mysql_real_escape_string($conn, $_POST['search']);
-    $sql = "SELECT * FROM users WHERE usersUid = $search";
-    $result = mysqli_query($conn, $sql);
-    $queryResult = mysqli_num_rows($result);
-}
-//profielen weergeven in vorm van link die overeenkomen met zoekterm 
-    if($queryResult > 0 ){
 
-        $row = mysqli_fetch_assoc($result);
+  header('location: '.$_POST["submit-search"].'')
+//profielen zoeken die overeenkomen met gegeven zoekterm 
+// if (isset($_POST['submit-search'])){
+//     $se
+//     $search = mysql_real_escape_string($conn, $_POST['search']);
+//     $sql = "SELECT * FROM users WHERE usersUid = $search";
+//     $result = mysqli_query($conn, $sql);
+//     $queryResult = mysqli_num_rows($result);
+// }
+// //profielen weergeven in vorm van link die overeenkomen met zoekterm 
+//     if($queryResult > 0 ){
+
+//         $row = mysqli_fetch_assoc($result);
         
-        header("Location: user.php?id={$row['userUid']}");
-        exit();
-    } else {
-        echo "There are no results matching your search";
-    }
+//         header("Location: user.php?id={$row['userUid']}");
+//         exit();
+//     } else {
+//         echo "There are no results matching your search";
+//     }
 ?>   

89a05adcd634527c4fedf8989a235fbebb5a27d1
isavdh <153298107+isavdh@users.noreply.github.com>
2024-02-01 17:21:18 +0100

    lasf

--- php_files/user.php
+++ php_files/user.php
@@ -104,7 +104,28 @@
             <?php } ?>
 
             <nav>
-                <a href="/html/friends.html">Friends</a> <span id ="counter"><?php include '/home/isah/public_html/webtech-2024/php_files/count_followers.php?id=' .$_GET['id']; ?></span>
+                <a href="/html/friends.html">Friends</a> <span id ="counter"><?php   
+    $usersUid= $_GET['id'];
+    $usersId = "SELECT usersId FROM users WHERE usersUid = '$usersUid'";
+    $result_usersId = $conn->query($usersId);
+
+    if ($result_usersId->num_rows>0){
+        $rowUserId = $result_userId->fetch_assoc();
+        $usersId = $rowUserId['usersId'];
+
+        $query_find_friends = "SELECT * FROM friends where user_ID_1 = $usersId";
+        $result_find_friends = $conn->query($query_find_friends);
+
+        $counter = $result_find_friends->num_rows;
+
+        
+        echo $counter;
+
+
+    } else {
+        
+        echo 'Could not find friends';
+    } ?></span>
             </nav> 
         </div>
 
